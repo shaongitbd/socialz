399d1bfe2a8e7df74c7f864dd71e83ee75372263
rabbisub <rabbi.cse.56005@sub.ac.bd>
2023-03-14 01:11:55 +0600

    regiser form with database connected

--- register.php
+++ register.php
@@ -4,25 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Regiter</title>
 </head>
 <body>
        <?php
+       $servername = "localhost";
+       $username = "root";
+       $password = "";
+       $database ="socialz";
+       $conn = mysqli_connect($servername,$username,$password,$database);
+       
     
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $username=$_POST["username"];
         $fname=$_POST["fname"];
         $lname=$_POST["lname"];
+        $gender=$_POST["gender"];
         $email=$_POST["email"];
         $phone_no=$_POST["phone_no"];
         $date_of_birth=$_POST["dof"];
         $address=$_POST["address"];
-        $username=$_POST["username"];
         $password=$_POST["password"];
 
 
+        $sql = "INSERT INTO user_info VALUES('$username','$fname','$lname','$gender','$email','$phone_no','$date_of_birth','$address','$password')";
         
     
-    
+        if (mysqli_query($conn, $sql)) {
+          echo "Data stored successfully.";
+              } 
+        else {
+          echo "Error: " . mysqli_error($sql);
+             }
     
     
     
@@ -34,7 +48,7 @@
        
 
 echo 
-'<center><form action="register.php" method="post">
+'<center><form class="" action="" method="post">
   <label for="fname">First name :</label><br>
   <input type="text" id="fname" name="fname" required><br><br>
 
@@ -42,14 +56,18 @@ echo
   <label for="lname">Last name :</label><br>
   <input type="text" id="lname" name="lname" required><br><br>
 
-  <form>
+  <label for="Gender">Gender :</label><br>
+  <input type="radio" id="gender" name="gender" value="Male" required>Male
+  <input type="radio" id="gender" name="gender" value="female" required>Female <br><br>
+
+ 
   <label for="fname">Email :</label><br>
   <input type="email" id="email" name="email" required><br><br>
 
   <label for="lname">Phone No :</label><br>
   <input type="text" id="phone_no" name="phone_no" required><br><br>
 
-  <form>
+  
   <label for="fname">Date of Birth :</label><br>
   <input type="date" id="dof" name="dof" required><br><br>
 
@@ -62,10 +80,9 @@ echo
   <label for="lname">Password : </label><br>
   <input type="password" id="password" name="password" autocomplete="off" required><br><br>
 
-  <input type="submit">
+  <input type="submit" name="register">
 
-</form>
-</form>
+
 </form></center>';
 
        }
