3d95f36e7f39c6af88d24f9f8c4338c246c19f61
rabbisub <rabbi.cse.56005@sub.ac.bd>
2023-03-26 12:25:07 +0600

    message.php update

--- message.php
+++ message.php
@@ -8,7 +8,12 @@ $result = mysqli_query($conn, $conversion_sql);
 $rows = [];
 while($row = mysqli_fetch_array($result))
 {
-    $rows[] = $row;
+    $id=$row["conversion_id"];
+    $sql = "SELECT to_friend,msg_time,msg_desc  FROM `message`  WHERE conversion_id=$id  ORDER BY msg_time LIMIT 1";
+    $res_one = mysqli_query($conn, $sql);
+    $msg=mysqli_fetch_array($res_one);
+
+    echo $msg["to_friend"];
 }
 echo mysqli_num_rows($result);
 echo $rows[0];
