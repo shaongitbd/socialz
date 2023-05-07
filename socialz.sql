d81f06a0daa53cc28bf2743f34547b260ad6e458
shaongitt <shaongit@gmail.com>
2023-05-07 15:31:44 -0400

    sql update

--- socialz.sql
+++ socialz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2023 at 09:18 PM
+-- Generation Time: May 07, 2023 at 09:31 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -52,7 +52,10 @@ CREATE TABLE `conversion` (
 
 INSERT INTO `conversion` (`conversion_id`, `friend_one`, `friend_two`) VALUES
 (4, 'rabbi', 'begumRokeya'),
-(5, 'rabbi', 'sakib');
+(5, 'rabbi', 'sakib'),
+(14, 'sakib', 'sakib'),
+(15, 'sakib', 'sakib'),
+(16, 'nazmul', 'nazmul');
 
 -- --------------------------------------------------------
 
@@ -71,8 +74,22 @@ CREATE TABLE `friends` (
 --
 
 INSERT INTO `friends` (`from_friend`, `to_friend`, `added_date`) VALUES
+('akashhalder', 'begumRokeya', '2023-05-07 19:12:53'),
+('akashhalder', 'rabbi', '2023-05-07 19:12:56'),
+('begumRokeya', 'akashhalder', '2023-05-07 19:12:53'),
+('begumRokeya', 'nazmul', '2023-05-07 19:06:59'),
+('begumRokeya', 'sakib', '2023-05-07 13:46:22'),
+('fahimfaisal', 'rabbi', '2023-05-07 19:15:33'),
+('nazmul', 'begumRokeya', '2023-05-07 19:06:59'),
+('nazmul', 'rabbi', '2023-05-07 19:10:13'),
+('rabbi', 'akashhalder', '2023-05-07 19:12:56'),
+('rabbi', 'fahimfaisal', '2023-05-07 19:15:33'),
+('rabbi', 'nazmul', '2023-05-07 19:10:13'),
 ('rabbi', 'sakib', '2023-04-04 07:59:27'),
-('sakib', 'rabbi', '2023-04-04 07:59:27');
+('rabbi', 'tasin', '2023-05-07 19:19:04'),
+('sakib', 'begumRokeya', '2023-05-07 13:46:22'),
+('sakib', 'rabbi', '2023-04-04 07:59:27'),
+('tasin', 'rabbi', '2023-05-07 19:19:04');
 
 -- --------------------------------------------------------
 
@@ -102,7 +119,11 @@ INSERT INTO `message` (`message_id`, `from_friend`, `to_friend`, `msg_desc`, `ms
 (28, 'rabbi', 'begumRokeya', 'What is your point?', '2023-04-03 23:49:17', 4),
 (34, 'rabbi', 'sakib', 'a', '2023-04-04 02:47:00', 5),
 (35, 'rabbi', 'sakib', 'Ki koros?', '2023-04-04 02:47:49', 5),
-(36, 'rabbi', 'sakib', 'kire ?', '2023-04-04 07:57:43', 5);
+(36, 'rabbi', 'sakib', 'kire ?', '2023-04-04 07:57:43', 5),
+(37, 'sakib', 'sakib', 'hello', '2023-05-07 13:46:55', 14),
+(38, 'sakib', 'sakib', '?', '2023-05-07 16:31:27', 14),
+(39, 'sakib', 'sakib', '>', '2023-05-07 18:08:45', 14),
+(40, 'nazmul', 'nazmul', 'apni  kemnon achen>?', '2023-05-07 19:07:28', 16);
 
 -- --------------------------------------------------------
 
@@ -148,7 +169,17 @@ INSERT INTO `status` (`status_id`, `status_owner`, `status_content`, `status_dat
 (14, 'rabbi', 'adad', '2023-03-29 14:37:29', 'images/status/pic.PNG'),
 (15, 'rabbi', 'How are u doing  right now?', '2023-03-29 15:36:55', 'images/status/pic.PNG'),
 (16, 'sakib', 'Thank you everyone', '2023-03-29 15:55:18', 'images/status/gray.jpg'),
-(17, 'begumRokeya', 'Chinso amay babaura?', '2023-03-29 17:41:40', 'images/status/Begum-Rokeya.jpg');
+(17, 'begumRokeya', 'Chinso amay babaura?', '2023-03-29 17:41:40', 'images/status/Begum-Rokeya.jpg'),
+(19, 'agga', 'what are u doin?', '2023-05-07 01:25:46', 'images/status/benz.png'),
+(20, 'sakib', '', '2023-05-07 03:02:14', ''),
+(21, 'sakib', 'adad', '2023-05-07 03:26:32', ''),
+(22, 'sakib', 'the dark night', '2023-05-07 18:41:16', 'images/status/lane_night_dark_139408_2560x1080.jpg'),
+(23, 'sakib', 'aa', '2023-05-07 18:47:04', 'images/status/gray.jpg'),
+(24, 'fahimfaisal', 'noukate bose chill pill', '2023-05-07 19:16:59', 'images/status/14324482_1096814920355309_2793467389593769525_o.jpg'),
+(25, 'tasin', 'vabuk mon', '2023-05-07 19:18:47', 'images/status/130268413_1808389805976564_7728801332392666972_n.jpg'),
+(26, 'rabbi', ':) great day ', '2023-05-07 19:20:13', 'images/status/335416615_1221502272085350_4866266929855674073_n.jpg'),
+(27, 'sakib', 'picnic day', '2023-05-07 19:22:15', 'images/status/323342984_738194827742391_9176065270690522609_n.jpg'),
+(28, 'akashhalder', 'শীত চলে গেলো যে,\r\nঢাকা ছেড়ে মুন্সিগঞ্জে।', '2023-05-07 19:23:50', 'images/status/329422656_548396793979576_1064017527336175259_n.jpg');
 
 -- --------------------------------------------------------
 
@@ -168,7 +199,22 @@ CREATE TABLE `status_comments` (
 --
 
 INSERT INTO `status_comments` (`status_id`, `comment_id`, `comment_owner`, `comment_desc`) VALUES
-(17, 1, 'sakib', 'wow');
+(17, 1, 'sakib', 'wow'),
+(15, 2, 'sakib', 'adadd'),
+(15, 3, 'sakib', 'adad'),
+(15, 4, 'sakib', 'is this wor'),
+(15, 5, 'sakib', 'nice post m'),
+(15, 6, 'sakib', 'nice poost '),
+(15, 7, 'sakib', 'adadadadada'),
+(15, 8, 'sakib', 'wtf is goin'),
+(15, 9, 'sakib', 'is this wor'),
+(13, 10, 'sakib', 'Hi '),
+(13, 11, 'sakib', 'thnaks'),
+(12, 12, 'sakib', 'thank you b'),
+(12, 13, 'sakib', 'thank to'),
+(12, 14, 'sakib', 'thank you b'),
+(17, 15, 'nazmul', 'hi nani'),
+(25, 16, 'rabbi', 'nice bro');
 
 -- --------------------------------------------------------
 
@@ -214,14 +260,18 @@ CREATE TABLE `user_info` (
 --
 
 INSERT INTO `user_info` (`username`, `first_name`, `last_name`, `gender`, `email`, `phone_no`, `date_of_birth`, `address`, `password`, `profile_pic`) VALUES
-('agga', 'fgad', 'gag', 'Male', 'rahelrabbihasan@gmail.com', 1518652714, '2023-03-06', 'forikabadh', '2432453', ''),
+('agga', 'fgad', 'gag', 'Male', 'rahelrabbihasan@gmail.com', 1518652714, '2023-03-06', 'forikabadh', '243c', ''),
+('akashhalder', 'Akash', 'Halder', 'male', 'akashhalder@gmail.com', 142852714, '2000-03-09', 'Barisal', '12345678', 'images/profile/342537319_136888182611861_9080054569899670518_n.jpg'),
 ('begumRokeya', 'Begum', 'Rokeya', 'Male', 'begum_nowonder@gmail.com', 929329323, '1942-03-06', 'Dhaka', '123456', 'images/profile/Begum-Rokeya.jpg'),
 ('demo26', 'adad', 'adad', 'ad', 'ea@gmail.com', 2432453, '2222-02-22', 'adad', 'dadad', ''),
+('fahimfaisal', 'Fahim', 'Faisal', 'male', 'fahimfaisal', 17637422, '2000-03-05', 'Gabtoli,Savar', '123456', 'images/profile/128806838_105240414783274_1895380407150855437_n.jpg'),
 ('gftygf', 'fhgv', 'jhvgcfg', 'Male', 'rahelrabbihasan@gmail.com', 1518652714, '2000-12-03', 'forikabadh', '1234567', ''),
+('nazmul', 'nazmul', 'Huda', 'male', 'nazmulhuda202@gmail.com', 151852714, '1999-11-11', 'sahanbag, GopalGanj', '12345678', 'images/profile/344391006_182364551388519_481637377131017651_n.jpg'),
 ('rabbi', 'rabbi', 'hasan', 'Male', 'rahelrabbihasan@gmail.com', 1518652714, '2001-03-15', 'forikabadh', '123456', 'images/profile/rabbi.jpg'),
 ('rabbit', 'hgfg', 'kjbjb', 'Male', 'hghv@gmail.com', 1518652714, '2005-12-12', 'forikabadh', '12345678', ''),
 ('sakib', 'sakib', 'ahmed', 'Male', 'sakib_khan@gmail.com', 1518652714, '2003-05-06', 'forikabadh', '12345678', 'images/profile/sakib.jpg'),
-('shaongit', 'MD', 'Sha', 'Male', 'shaongit@gmail.com', 18343212, '2000-07-11', 'Dhaka', '12345', '');
+('shaongit', 'MD', 'Sha', 'Male', 'shaongit@gmail.com', 18343212, '2000-07-11', 'Dhaka', '12345', ''),
+('tasin', 'Tasin', 'Mahmud', 'male', 'tashinmahmud@gmail.com', 18387783, '1999-03-03', 'Barisal', '12345678', 'images/profile/142306845_1847615768720634_3739833070165101284_n.jpg');
 
 --
 -- Indexes for dumped tables
@@ -309,25 +359,25 @@ ALTER TABLE `comment_likes`
 -- AUTO_INCREMENT for table `conversion`
 --
 ALTER TABLE `conversion`
-  MODIFY `conversion_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `conversion_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `status`
 --
 ALTER TABLE `status`
-  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `status_comments`
 --
 ALTER TABLE `status_comments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `status_likes`
