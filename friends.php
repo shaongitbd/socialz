848a6b1d9c888f5bc74dbf2514b481e7c771f0ff
shaongitt <shaongit@gmail.com>
2023-05-08 07:44:53 -0400

    updated bugs

--- friends.php
+++ friends.php
@@ -17,12 +17,9 @@ if (isset($_GET["add"])){
 
    $friend = $_GET['add'];  
   
-  
-  $step_one = "insert into `friends`(from_friend, to_friend,added_date) VALUES ('$username', '$friend',current_timestamp())";
+   $step_one = "insert into `pending_request`(from_friend, to_friend,request_by) VALUES ('$username', '$friend','$username')";
   $step_one_q = mysqli_query($conn, $step_one);
 
-  $step_two = "insert into `friends`(from_friend, to_friend,added_date) VALUES ('$friend','$username', current_timestamp())";
-  $step_two_q = mysqli_query($conn, $step_two);
   $friends_list = "SELECT first_name, last_name,profile_pic,username ,to_friend,added_date FROM `friends` INNER JOIN `user_info` ON friends.to_friend=user_info.username WHERE from_friend='$username'";
   $result = mysqli_query($conn, $friends_list);
   
@@ -42,6 +39,55 @@ $result = mysqli_query($conn, $friends_list);
 
 
 }
+
+
+if (isset($_GET["accept"])){
+
+   $friend = $_GET['accept'];  
+  
+  $remove_sql =   "UPDATE `pending_request` SET `status` = 'accept' WHERE `pending_request`.`from_friend` = '$friend' AND `pending_request`.`to_friend` = '$username'";
+
+  $remove_res = mysqli_query($conn, $remove_sql);
+  $remove_pending = "DELETE  FROM `pending_request` WHERE from_friend = '$friend' and to_friend = '$username'";
+  $remove_pending_exe = mysqli_query($conn, $remove_pending);
+
+  $friends_list = "SELECT first_name, last_name,profile_pic,username ,to_friend,added_date FROM `friends` INNER JOIN `user_info` ON friends.to_friend=user_info.username WHERE from_friend='$username'";
+  $result = mysqli_query($conn, $friends_list);
+  
+  
+  }
+
+
+$pending_request_sql = "SELECT first_name, last_name,profile_pic,username ,to_friend,from_friend FROM `pending_request` INNER JOIN `user_info` ON pending_request.from_friend=user_info.username   WHERE to_friend = '$username' and request_by != '$username'  ";
+$pending_request_exe = mysqli_query($conn, $pending_request_sql);
+
+
+
+
+echo '<h1 class="mt-10 text-sm text-gray-600 font-semibold ">Pending Request </h1><div class="max-w-sm">';
+while($pen_req = mysqli_fetch_array($pending_request_exe))
+{
+   ////username url 
+
+echo'
+
+
+<div class=" flex flex-row justify-content my-7 ">
+<img class=" rounded-full" src="'.$pen_req["profile_pic"].'"   width=80 height=80/>
+<div class="flex flex-col w-1/4 mx-4 ">
+ <a class="text-sm text-gray-500 font-semibold  " href=" /profile.php?username='.$pen_req["from_friend"].'">'.$pen_req["first_name"].'  '.$pen_req["last_name"].'</a>
+ <a  href="friends.php?accept='.$pen_req["from_friend"].'" class="mt-4 text-sm px-2 py-1.5 bg-red-800 text-semibold text-white rounded-md "> Accept </a>
+ </div>
+ </div>
+
+
+
+
+   ';
+}
+
+echo'</div>';
+
 
 
 
